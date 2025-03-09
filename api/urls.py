17784ab7aad057a772d07a70e59d497efdc693b0
Rovrtoji <157040235+Rovrtoji@users.noreply.github.com>
2025-03-08 21:09:55 -0600

    agrega el path

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import get_records, create_record_view, update_record_view, delete_record_view
+from .views import get_records, create_record_view, update_record_view, delete_record_view, register_odoo_instance
 
 urlpatterns = [
     path('get_records/', get_records, name='get_records'),
     path('create_record/', create_record_view, name='create_record'),
     path('update_record/', update_record_view, name='update_record'),
     path('delete_record/', delete_record_view, name='delete_record'),
+    path('register_odoo_instance/', register_odoo_instance, name='register_odoo_instance'),
 ]
